4d0881fe32b0bb4bba4597c5acf9594f8edd9043
Ian Duffy <ianduffy@Ians-MacBook-Pro.local>
2024-11-13 18:40:00 +0000

    added article list from sanity

--- src/queries/articles.ts
+++ src/queries/articles.ts
@@ -1,11 +1,35 @@
+import type { PortableTextBlock, Slug } from 'sanity';
 import { sanityClient } from 'sanity:client';
+
+export interface Article {
+	_createdAt: string;
+	_id: string;
+	_rev: string;
+	_type: string;
+	_updated: string;
+	description: string;
+	slug: Slug;
+	title: string;
+	estimatedReadingTime: number | null;
+	content: PortableTextBlock[];
+	publishedDate: string;
+}
+
+export const getAllArticlesSlug = async (): Promise<
+	{ params: { slug: string } }[]
+> => {
+	const articlesSlugs = await sanityClient.fetch(
+		`*[_type == "articles"]{"params": {"slug": slug.current}}`
+	);
+	return articlesSlugs;
+};
 
 export const allArticlesQuery = `*[_type == "articles"] { 
   ...,
   "estimatedReadingTime": round(length(pt::text(content)) / 5 / 180 )
 } |  order(publishedDate desc)`;
 
-export const getAllArticles = async () => {
+export const getAllArticles = async (): Promise<Article[]> => {
 	const articles = await sanityClient.fetch(allArticlesQuery);
 
 	return articles;
@@ -22,7 +46,7 @@ export const articleFromSlugQuery = `*[_type == "articles" && slug.current == $s
   }
 }`;
 
-export const getArticleFromSlug = async (slug: string) => {
+export const getArticleFromSlug = async (slug: string): Promise<Article> => {
 	const article = await sanityClient.fetch(articleFromSlugQuery, { slug });
 
 	return article;
