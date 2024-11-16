174782d53f530998ca1afb2c09bd6c42ae01a06a
Ian Duffy <ianduffy@Ians-MacBook-Pro.local>
2024-11-16 13:21:19 +0000

    add contents table and article styling

--- src/features/article/components/article-body/custom-article-components/react/custom-css-buttons/CustomCSSButtons.tsx
+++ src/features/article/components/article-body/custom-article-components/react/custom-css-buttons/CustomCSSButtons.tsx
@@ -1,7 +1,7 @@
-
+import CSS from './styles.module.scss'
 export function CssVarButtons () {
   return (
-    <div className='flex gap-2 justify-center w-full my-10 flex-wrap no-gap-x' style={{ '--gap': '0.5rem' } as React.CSSProperties}>
+    <div className={CSS.container}>
       <button className='button'>Basic Button</button>
       <button className='button success'>Submit Button</button>
       <button className='button danger'>Cancel Button</button>
