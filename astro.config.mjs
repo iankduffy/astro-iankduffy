7d78c48f35ec073d2d9367b46d6b232c69ac17c8
Ian Duffy <ianduffy@Ians-MacBook-Pro.local>
2025-07-08 19:43:00 +0100

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,36 +4,40 @@ import { defineConfig } from 'astro/config';
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 const { SANITY_PROJECT_ID, SANITY_DATASET, SANITY_API_VERSION } = loadEnv(
-	process.env.NODE_ENV || '',
-	process.cwd(),
-	''
+    process.env.NODE_ENV || '',
+    process.cwd(),
+    ''
 );
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://iankduffy.com',
-	output: 'static',
-	devToolbar: {
-		enabled: false,
+  site: 'https://iankduffy.com',
+  output: 'static',
+
+  devToolbar: {
+      enabled: false,
 	},
-	// trailingSlash: 'ignore',
-	integrations: [
-		sanity({
-			projectId: SANITY_PROJECT_ID,
-			dataset: SANITY_DATASET,
-			useCdn: true,
-			apiVersion: SANITY_API_VERSION,
-			// perspective: 'previewDrafts',
-			// token: process.env.SANITY_DRAFT_KEY,
-		}),
-		react({
-			include: ['**/react/*'],
-		}),
-		sitemap({
-			filter: (page) =>
-				page !== 'https://iankduffy.com/uses/' &&
-				page !== 'https://iankduffy.com/snippets/',
-		}),
+
+  integrations: [
+      sanity({
+          projectId: SANITY_PROJECT_ID,
+          dataset: SANITY_DATASET,
+          useCdn: true,
+          apiVersion: SANITY_API_VERSION,
+          // perspective: 'previewDrafts',
+          // token: process.env.SANITY_DRAFT_KEY,
+      }),
+      react({
+          include: ['**/react/*'],
+      }),
+      sitemap({
+          filter: (page) =>
+              page !== 'https://iankduffy.com/uses/' &&
+              page !== 'https://iankduffy.com/snippets/',
+      }),
 	],
+
+  adapter: netlify(),
 });
